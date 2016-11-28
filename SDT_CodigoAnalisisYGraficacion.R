478e70dbba6a3adf9852cc95c491546eb764e7a9
Adriana Chavez <adrifelcha@gmail.com>
2016-11-28 15:23:55 -0600

    Codigo para hacer graficas y analisis correspondiente a SDT, a partir de una tasa de hits y F.A (ROC included)

--- SDT_CodigoAnalisisYGraficacion.R
+++ SDT_CodigoAnalisisYGraficacion.R
@@ -1,9 +1,13 @@
 #Codigo Base para Analisis Parametrico y Graficacion (SDT)
+#Autor: Adriana F. Chávez De la Peña
+#adrifelcha@gmail.com
+##########################################################
+
 rm(list=ls())  #Limpiamos el espacio de trabajo
 
 #####Datos a graficar#####
 h<-123  #Numero crudo de Hits
-f<-75   #Numero de Falsas Alarmas
+f<-32   #Numero de Falsas Alarmas
 h_rate<-h/150      #Tasa de Hits relativo al total de Ensayos con Se??al
 fa_rate<-f/150     #Tasa de Falsas Alarmas relativa al total de Ensayos con Ruido
 
@@ -33,4 +37,42 @@ text(-5,0.34,paste("C = ",c))  #Imprimimos el valor del Sesgo C
 text(-4,0.29,paste("D' = ",d)) #Imprimimos el valor de la distancia entre las medias (D')
 text(-5,0.24,paste("Hit Rate = ",h_rate))            #Especificamos la Tasa de Hits
 text(-4,0.19,paste("False alarm Rate = ",fa_rate))   #Especificamos la Tasa de Falsas Alarmas
+
+
+############ Curvas ROC
+
+# Una vez que hemos computado el valor de la d' que separa las distribuciones de ruido y señal
+# podemos trazar una curva ROC, que nos describa todas las combinaciones posibles de tasa de
+# Hits y Falsas Alarmas, dada todas las posibles combinaciones del criterio. 
+
+#Para trazar la Curva ROC, necesitamos crear un Ciclo For:
+
+
+hits <- c()   #Creamos un arreglo vacío, que vamos a ir llenando con el Ciclo For, para las tasas de Hits
+falarms <- c()  #Creamos un arreglo vacío para las tasas de Falsas Alarmas
+bias_c <- seq(-10,10,0.1) 
+d_null <- 0  #Como referencia, vamos a incluir una curva ROC para una d' de 0. El sistema a evaluar será juzgado como 'más preciso' conforme su ROC se aleje de ésta función de identidad.
+hits_na <- c()     #Creamos un arreglo vacío para los hits en  d' 0
+falarms_na <- c()  # Creamos un arreglo vacío para las falsas alarmas en d' 0
+
+
+for (i in 1:length(bias_c)){                     #Creamos un For donde, para cada posible valor del sesgo C (que relaciona directamente d' con el criterio)
+  hits[i] <- pnorm((-d/2)-bias_c[i])             #se compute la proporción de la distribución de señal que cae sobre el criterio
+  falarms[i] <- pnorm((d/2)-bias_c[i])           # y la proporción de la distribución de ruido.
+  hits_na[i] <- pnorm((d_null/2)-bias_c[i])      #Para referencia, realizamos el mismo cómputo para la d' de 0
+  falarms_na[i] <- pnorm((-d_null/2)-bias_c[i])
+}
+
+plot(fa_rate,h_rate, pch=16, col='deepskyblue4', xlim=c(0,1), ylim=c(0,1), xlab='F.A. Rate', ylab='Hit Rate')     #Ploteamos las tasas de hits y falsas alarmas obervadas como un punto en el espacio
+lines(hits,falarms,lwd=2,col='deepskyblue2')        #Dibujamos la curva ROC correspondiente a la d' de nuestro sistema evaluado
+lines(hits_na,falarms_na,lwd=1,col='black', lty=2)  #Dibujamos la función de identidad, que corresponde a una d' de 0 (Donde las distribuciones de ruido y señal se empalman por completo)
+lines(c(0.38, 0.48),c(0.2,0.2), lwd=2, lty=1, col="deepskyblue3")      
+points(0.43,0.1, lty=3, pch=16, col='deepskyblue4')
+text(0.5, 0.2, labels="All possible trade-offs between Hit & F.A. rates given the d'", offset=0, cex = 0.7, pos=4)
+text(0.5, 0.1, labels="Observed Hit & F.A. rates given the used criterion", offset=0, cex = 0.7, pos=4)
+text(0.85, 0.83, labels="d' = 0", offset=0, cex = 0.8, pos=4)
+text(fa_rate-0.13, h_rate+0.02, paste("d' =", d), offset=0, cex = 0.8, pos=4)
+title('ROC')
+
+
 
