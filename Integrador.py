c8c5718db9a7b9c5b22a7d2e908a41b43a0664ac
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2016-12-13 13:22:32 -0600

    Adición de algunos comentarios en el codigo del Integrador en Python

--- Integrador.py
+++ Integrador.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 ### Programa en Python Integrador ###
 from matplotlib.widgets import Slider
 import matplotlib.pyplot as plt
@@ -6,18 +7,19 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.1, bottom=0.25)   
 
-a = 0.55
-
+# Creamos un arreglo de 30 elelmentos, el primer elemento tiene un valor de 100
+# (representa un estimulo en el tiempo) el resto tiene un valor de 0.  
 R = [100]*1 + [0]*29
-x = [0]*31
-y = [0]*31
-V_ant = 0
+x = range(31)			# Creamos un arreglo de 31 elementos [0, 1, 2, 3, 4....]
+y = [0]*31				# Creamos un arreglo que llenaremos con el resultado de la función del integrador
+						# Por lo pronto lo llenaremos con ceros
 
-x[0] = 0.0
-y[0] = 0.0
+a = 0.55				# Declaramos el parametro de Ventana Temporal
+y[0] = 0.0				 
+V_ant = 0.0
 
-for i in range(len(R)):
-	x[i+1] = i+1
+# Calculamos la función integrador y lo guardamos en la variable Y[]
+for i in range(len(R)):	# Este ciclo For, se recorrera para todo el largo del arreglo R[]
 	y[i+1] = a*V_ant + (1-a)*R[i]
 	V_ant = y[i+1]
 
@@ -35,7 +37,6 @@ def update(val):
 	a = a_value.val
 	V_ant = 0
 	for i in range(len(R)):
-		x[i+1] = i + 1
 		y[i+1] = a*V_ant + (1-a)*R[i]
 		V_ant = y[i+1]
 	ax.clear()
