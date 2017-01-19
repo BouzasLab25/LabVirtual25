9132015a2eea37f6a3987fbfa70200d2b3ace295
Stephane Lejars <lejarsc.s@gmail.com>
2017-01-19 14:33:28 -0600

    categorización

---
+++ Arousal/Reflejos.py
@@ -0,0 +1,23 @@
+import math
+import numpy                      #Biblioteca para operaciones matematicas complejas
+from matplotlib.widgets import Slider, Button  #Bajamos la libreria para hacer Sliders
+import matplotlib.pyplot as plt #Bajamos la libreria para hacer graficas
+
+tiempo = range(1000)  #Genera arreglo de 0 a 99<
+respuesta = []
+A = 14.5
+alfa = 315.0
+
+for i in tiempo:
+    respuesta.append(A*math.exp(-tiempo[i]/alfa))
+
+fig, ax = plt.subplots(2)
+max_time = tiempo[len(tiempo) - 1] + 1
+max_resp = numpy.amax(respuesta) + 1;
+ax[1].set_yscale('log') #Fijar escala logaritmica
+ax[0].axis([0,max_time,-1, max_resp]) #Fijar limites de los ejes
+ax[1].axis([0,max_time, 1, max_resp]) 
+ax[0].plot(tiempo, respuesta)  #La grafica en si
+ax[1].plot(tiempo, respuesta)
+plt.show();
+
