1b54a85f16e11fd997b991f80d8966405dba8500
JNinoGarc <jninogarc@gmail.com>
2017-02-07 19:38:16 -0600

    Versiones para comentar

--- Reflejos/Reflejos.py
+++ Reflejos/Reflejos.py
@@ -3,13 +3,18 @@ import numpy                      #Biblioteca para operaciones matematicas compl
 from matplotlib.widgets import Slider, Button  #Bajamos la libreria para hacer Sliders
 import matplotlib.pyplot as plt #Bajamos la libreria para hacer graficas
 
-tiempo = range(100)  #Genera arreglo de 0 a 99<
+tiempo = range(300)  #Genera arreglo de 0 a 99<
 respuesta = []
-A = 1000.0
-alfa = 1.0
+B = 14.5
+b= ([60,120,180,240,300,360], [17,18,15,14,13,10])
+C = 12.5
+c= ([60,120,180,240,300], [13,14,14,14,13,13,12,11,11,10])
+D =  9
+d= ([0,60,120,180,240,300], [10,9,7,8,5,6])
+alfa = 30
 
 for i in tiempo:
-    respuesta.append(A*math.exp(-tiempo[i]/alfa))
+    respuesta.append(D*math.exp(-tiempo[i]/alfa))
 
 fig, ax = plt.subplots(2)
 max_time = tiempo[len(tiempo) - 1] + 1
@@ -19,5 +24,8 @@ ax[0].axis([0,max_time,-1, max_resp]) #Fijar limites de los ejes
 ax[1].axis([0,max_time, 1, max_resp]) 
 ax[0].plot(tiempo, respuesta)  #La grafica en si
 ax[1].plot(tiempo, respuesta)
-plt.show();
+ax[0].scatter([0,60,120,180,240,300], [10,9,7,8,5,6])
+ax[0].set_title('Respuestas por minuto ', fontsize=14, fontweight='bold') 
+ax[1].set_title('', fontsize=18, fontweight='bold') 
 
+plt.show();
