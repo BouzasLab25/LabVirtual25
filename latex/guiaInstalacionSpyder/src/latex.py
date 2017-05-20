2b50ea1256531aa60fd7454c5cefb3ecbc751dc1
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2017-05-20 14:25:09 -0500

    Reacomodo de la carpetas de latex, inclusión de documento Introducción a Python.

--- latex/guiaInstalacionSpyder/src/latex.py
+++ latex/guiaInstalacionSpyder/src/latex.py
@@ -2,11 +2,12 @@
 
 import os, sys
 
-commands = ["pdflatex " + sys.argv[1] + ".tex",
+commands = [
+    "pdflatex " + sys.argv[1] + ".tex",
     "bibtex " + sys.argv[1] + ".aux",
     "pdflatex " + sys.argv[1] + ".tex",
     "pdflatex " + sys.argv[1] + ".tex",
-    "mv " + sys.argv[1] + ".pdf ../.."
+    "mv " + sys.argv[1] + ".pdf ../pdf"
 ]
 
 for c in commands:
---
+++ latex/pythonIntroduccion/src/latex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os, sys
+
+commands = [
+    "pdflatex " + sys.argv[1] + ".tex",
+    "bibtex " + sys.argv[1] + ".aux",
+    "pdflatex " + sys.argv[1] + ".tex",
+    "pdflatex " + sys.argv[1] + ".tex",
+    "mv " + sys.argv[1] + ".pdf ../pdf"
+]
+
+
+for c in commands:
+    os.system(c)
