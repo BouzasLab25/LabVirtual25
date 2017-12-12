c79abfcd5ed496dd314f750e1a5e13554c905db0
Adriana Chavez <adrifelcha@gmail.com>
2017-12-11 19:07:28 -0600

    App de Distribuciones Normales

--- NormalCurve.R
+++ NormalCurve.R
@@ -2,30 +2,24 @@
 
 
 ui <- fluidPage(
-                fluidRow(
-                  column(8, offset =6, tags$h1("Adriana F Chavez - Lab25"))),
-                tags$img(height=100, 
-                         width=100,
-                         src = "http://www.thinkgeek.com/images/products/additional/large/jgpo_poke_ball_serving_bowl_set_closed.jpg"),
-                tags$h3("Learning R Shiny"),
-                tags$p( "This is my very", tags$strong("FIRST"),"Shiny app"),
-                tags$a(href = "https://adrifelcha.github.io/", "My Github Page"),
-                tags$hr(),
-                actionButton(inputId="click", label ="Click me!"),
-                wellPanel(textInput(inputId = "titulo",
-                          label = "Titulo", 
-                          value = "Histograma"),
-                textInput(inputId = "evidence",
-                          label = "Datos", 
-                          value = "Variable"),
-                sliderInput(inputId="num", 
-                            label = "Choose a number",
-                            value=25, min=1, max=100),
-                actionButton(inputId="go", label ="Update Graph")),
-                fluidRow(
-                  column(6, offset = 3, plotOutput(outputId="hist"))),
-                fluidRow(
-                  column(6, offset = 3, verbatimTextOutput("stats"))))
+  fluidRow(HTML('<h1 style="text-align:center;"><b>Distribución  Normal</b></h1>'),
+           HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')), 
+  
+  fluidRow(HTML('<hr style="border: 0; height: 1px; background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);"')),
+  
+  fluidRow(column(width=5, offset = 0.6, background = 'yellow',
+                  HTML('<h1 style="text-align:center; color:green;">Definición</h1>'),
+                  HTML('<hr style="border: 0; height: 1px; background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);"'),
+                  tags$p("También llamada Curva Normal o Campana de Gauss, una distribución de probabilidad Normal esta definida por los siguientes parámetros:"),
+                  tags$br(),
+                  wellPanel(sliderInput(inputId="mean", 
+                    label = "Media",
+                    value=25, min=-100, max=100),
+                    sliderInput(inputId="dev", 
+                    label = "Desviación Estándar",
+                    value=25, min=1, max=100),
+                  actionButton(inputId="go", label ="Update Graph"))),
+            column(width=5, offset = 6, plotOutput(outputId="hist"), verbatimTextOutput("stats"))))
 
 server <- function(input, output) {
   data <- eventReactive(input$go, {rnorm(input$num)})
