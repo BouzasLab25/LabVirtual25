19f52ea46ceb170ccfe52860334f12b978e0669e
Adriana Chavez <adrifelcha@gmail.com>
2018-01-11 14:35:51 -0600

    Graficador SDT

--- Apps Felisa/SDT.R
+++ Apps Felisa/SDT.R
@@ -1,16 +1,16 @@
 library(shinydashboard)
 
 ui <- dashboardPage(
-          dashboardHeader(
-                          disable = TRUE),
-          dashboardSidebar(
-            width = 190,
-            sidebarMenu(
-                menuItem("Definición", tabName = "defin", icon = icon("book")),
-                menuItem("La media", tabName = "media", icon = icon("star-o")),
-                menuItem("Desviación Estándar", tabName="desv", icon = icon("star-o")),
-                menuItem("Propiedades", tabName="prop", icon = icon("book")),
-                menuItem("Glosario", tabName="Glos", icon = icon("info")))),
+  dashboardHeader(
+    disable = TRUE),
+  dashboardSidebar(
+    width = 190,
+    sidebarMenu(
+      menuItem("Introducción", tabName = "intro", icon = icon("book")),
+      menuItem("Sobre el modelo", tabName = "about", icon = icon("star-o")),
+      menuItem("Curvas ROC", tabName="roc", icon = icon("star-o")),
+      menuItem("Análisis No Paramétrico", tabName="nop", icon = icon("book")),
+      menuItem("Estimación Paramétrica", tabName="para", icon = icon("info")))),
   dashboardBody(
     tags$head(
       tags$style(HTML('.main-header .logo {
@@ -20,12 +20,14 @@ ui <- dashboardPage(
                       
                       .body{
                       background-color:green;}
+
                       .js-irs-0 .irs-bar {
                       border-top-color: #000000; border-bottom-color: #000000;} 
                       .js-irs-0 .irs-bar-edge {
                       border-color#000000;}
                       .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {
                       background: #AC58FA;}
+
                       .js-irs-1 .irs-bar {
                       border-top-color: #000000; border-bottom-color: #000000;} 
                       .js-irs-1 .irs-bar-edge {
@@ -34,37 +36,53 @@ ui <- dashboardPage(
                       background: #9F81F7;}'))),
     tabItems(
       # First tab content
-      tabItem(tabName = "defin",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Distribución  Normal</u></b></h1>'),
+      tabItem(tabName = "intro",
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teoría de Detección de Señales</u></b></h1>'),
                        HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')),
               
               fluidRow(column(width=5, offset = 1, background = 'yellow',
-              #fluidRow(box(   
-                              HTML('<h1 style="text-align:center; color:purple;"><b>Definición</b></h1>'),
+                              #fluidRow(box(   
+                              HTML('<h1 style="text-align:center; color:purple;"><b>Introducción</b></h1>'),
                               HTML('<hr style="border: 0; height: 1px; background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);"'),
-                              HTML('<h3 style="text-align:justify;"> También llamada <b>Curva Normal</b> o <b>Campana de Gauss</b>, una distribución de probabilidad Normal está definida por los siguientes parámetros:</h3>'),
-                              tags$br(),
-                              tags$br(),
+                              HTML('<h4 style="text-align:justify;"></h4>'),
+                              HTML('<h4 style="text-align:justify;"> La <b>Teoría de Detección de Señales</b> (<b>SDT</b>, por sus siglas en inglés), describe el problema de decisión al que se enfrentan los orgamismos cuando, en un entorno con incertidumbre, tienen que decidir si <i>algo</i> (la <b>señal</b>) está o no presente, para poder actuar en consecuencia.</h4>'),
+                              HTML('<h4 style="text-align:justify;"> El desarrollo de la SDT gira en torno a dos grandes supuestos: <b>1) Siempre hay algún grado de incertidumbre</b> al detectar algo, por tres grandes razones: 1-nuestros sistemas perceptuales son imperfectos, 2- toda señal a detectar se presenta en el mundo con cierta variabilidad y 3- las señales coexisten en el mundo con <b>ruido</b>: estímulos que sin ser la señal, pueden llegar a confundirse con esta; y  <b>2) Las consecuencias importan</b>, acertar en el juicio de detección paga, errar cuesta y ambas cosas ocurren en distinta magnitud.</h4>'),
+                              HTML('<h4 style="text-align:justify;"> De acuerdo con la <b>TDS</b>, los organismos ponderan la información que poseen sobre la estructura de su entorno para compensar la incertidumbre contenida en la tarea (<i>representada gráficamente como el área de sobrelape entre las distribuciones de probabilidad del ruido y la señal</i>), fijando un <b>criterio de elección</b> (<i>la línea vertical que corta ambas distribuciones a la altura de un valor (x)</i>) que va a fungir como punto de corte para determinar a partir de cuánta evidencia se juzgará la presencia de la señal.</h4>'),
+                              HTML('<h4 style="text-align: justify;">En función a la correspondencia entre la presencia o asuencia de la señal en el mundo y el juicio de elección emitido por el organismo, podemos identificar dos grandes tipos de aciertos y errores. </h3>'),
+                              plot(2,3),
+                              HTML('<h2 style="text-align:center; color:purple;">Por Ejemplo:</h2>'),
+                              fluidRow(box(HTML('<h4>a) Si yo sé cuál es  <b>la estatura promedio</b> de las mujeres en mi familia, ¿qué predicciones puedo hacer sobre la estatura que tendrá mi sobrina recién nacida cuando crezca?.</h4>')),
+                                       box(tags$br(),
+                                           HTML('<h4>b) Más específicamente, ¿Qué tan probable es que mi sobrina llegue a medir mas de 1.70?</h4>'),
+                                           tags$br())),
                               wellPanel(sliderInput(inputId="mean", 
-                                                    label = "La media",
-                                                    value=0, min=-5, max=5),
+                                                    label = "Media",
+                                                    value=0, min=1.60, max=1.80,
+                                                    step= 0.05),
                                         sliderInput(inputId="dev", 
                                                     label = "Desviación Estándar",
-                                                    value=3, min=1, max=5)),
-                              HTML('<h4 style="text-align: justify;">Se trata de una <b>distribución de probabilidad continua</b> que describe la probabilidad de que se observe cierto <b>valor (x)</b> si extraigo <b>aleatoriamente</b> un elemento perteneciente a cierta <b>muestra</b> o <b>población</b>.</h3>'),
-                              HTML('<h4 style="text-align: justify;">La Distribución Normal es muy utilizada en las ciencias sociales y naturles, pues permiten describir la forma en que los <b>valores</b> observados en ciertas <b>variables</b> se distribuyen al rededor de su <b>media</b> (<i>a.k.a. el promedio de las observaciones recopiladas</i>).</h3>')),
+                                                    value=3, min=.01, max=.1,
+                                                    step=.02))),
                        column(width=5, offset = 0, 
                               tags$br(),
                               tags$br(),
                               tags$br(),
                               tags$br(),
-                              plotOutput(outputId="normal"), verbatimTextOutput("stats")))
-                       #box(plotOutput(outputId="normal"), verbatimTextOutput("stats")))
-              ),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              tags$br(),
+                              plotOutput(outputId="normal")))
+              #box(plotOutput(outputId="normal"), verbatimTextOutput("stats")))
+      ),
       
       # Second tab content
       tabItem(tabName = "media",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Distribución  Normal</u></b></h1>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teoría de Detección de Señales</u></b></h1>'),
                        HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')),
               
               fluidRow(column(width=5, offset = 1, background = 'yellow',
@@ -103,7 +121,7 @@ ui <- dashboardPage(
       ),
       
       tabItem(tabName = "desv",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Distribución  Normal</u></b></h1>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teoría de Detección de Señales</u></b></h1>'),
                        HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')),
               fluidRow(column(width=5, offset = 1, background = 'yellow',
                               HTML('<h1 style="text-align:center; color:purple;"><b>La Desviación Estándar</b></h1>'),
@@ -142,11 +160,11 @@ ui <- dashboardPage(
                               HTML('<h4 style="text-align: justify;">La media funciona como una <b>medida de tendencia central</b> que permite describir en torno a qué valor se distribuyen las observaciones de cierta variable.</h3>'),
                               HTML('<h4 style="text-align: justify;">La Distribución Normal es muy utilizada en las ciencias sociales y naturles, pues permiten describir la forma en que los <b>valores</b> observados en ciertas <b>variables</b> se distribuyen al rededor de su <b>media</b> (<i>a.k.a. el promedio de las observaciones recopiladas</i>).</h3>')),
                        column(width=5, offset = 0, plotOutput(outputId="desviacion")))
-            
+              
       ),
       
       tabItem(tabName = "prop",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Distribución  Normal</u></b></h1>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teoría de Detección de Señales</u></b></h1>'),
                        HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')),
               fluidRow(column(width=5, offset = 1, background = 'yellow',
                               HTML('<h1 style="text-align:center; color:purple;"><b>Propiedades</b></h1>'),
@@ -171,18 +189,17 @@ ui <- dashboardPage(
               )
       )
     )
-  )
-)
+      )
+      )
 
 server <- function(input, output) {
-  distribucion <- reactive({dnorm(seq(-15,15,.05),input$mean,input$dev)})
-  output$normal <- renderPlot({plot(seq(-15,15,.05), distribucion(), main="", xlab="", ylab="",type='l',
-                                    font.lab=2, axes = "FALSE", xlim= c(-15,15), col="purple")
-    axis(1,at=c(-14,15, 10, 7, 5, 4, 3, 2, 1, 0, -10, -7, -5, -4, -3, -2, -1),labels=c("-Infinito", "Infinito", "10", "7", "5", "4", "3", "2", "1", "0", "-10", "-7", "-5", "-4", "-3", "-2", "-1"), font=2)
+  distribucion <- reactive({dnorm(seq(0.7,2.4,.001),input$mean,input$dev)})
+  output$normal <- renderPlot({plot(seq(0.7,2.4,.001), distribucion(), main="", xlab="", ylab="",type='l',
+                                    font.lab=2, axes = "FALSE", xlim= c(1.25,2.1), col="purple")
+    axis(1,at=c(1.5, 1.6, 1.7, 1.8, 1.9, 2),labels=c("1.5", "1.6", "1.7", "1.8", "1.9", "2"), font=2)
     abline(v=input$mean, lwd=2)       
     text(input$mean+.9,.05,expression(paste(mu)),cex=2.5,col='black',f=2)
-    text(1.9,(.2),"Hola",cex=1.2,col='white',f=2)
-    mtext("Valores'",1,cex=1.3, line=3, f=2)})
+    mtext("Estatura de las mujeres en mi familia",1,cex=1.3, line=3, f=2)})
   output$stats <- renderPrint({summary(distribucion())})
   
   valores <- reactive({c(input$val_1,input$val_2,input$val_3,input$val_4,input$val_5)})
