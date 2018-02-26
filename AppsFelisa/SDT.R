a6f831d9ddd73c1b2e1cbce5f07332efb4ff6547
Adrifelcha <adrifelcha@gmail.com>
2018-02-25 23:42:11 -0600

    Codificando acentos

--- AppsFelisa/SDT.R
+++ AppsFelisa/SDT.R
@@ -6,11 +6,11 @@ ui <- dashboardPage(
   dashboardSidebar(
     width = 190,
     sidebarMenu(
-      menuItem("Introducción", tabName = "intro", icon = icon("book")),
+      menuItem("Introducci\u{00F3}n", tabName = "intro", icon = icon("book")),
       menuItem("Sobre el modelo", tabName = "about", icon = icon("star-o")),
       menuItem("Curvas ROC", tabName="roc", icon = icon("star-o")),
-      menuItem("Analisis No Paramétrico", tabName="nop", icon = icon("book")),
-      menuItem("Estimación Paramétrica", tabName="para", icon = icon("info")))),
+      menuItem("Analisis No Param\u{00E9}trico", tabName="nop", icon = icon("book")),
+      menuItem("Estimaci\u{00F3}n Param\u{00E9}trica", tabName="para", icon = icon("info")))),
   dashboardBody(
     tags$head(
       tags$style(HTML('.main-header .logo {
@@ -37,18 +37,18 @@ ui <- dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "intro",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teoría de Detección de Señales</u></b></h1>'),
-                       HTML('<p style="text-align:center;"><b>por Adriana F. Chávez</b></p>')),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>Teor&iacutea de Detecci&oacuten de Se&ntildeales</u></b></h1>'),
+                       HTML('<p style="text-align:center;"><b>por Adriana F. Ch&aacutevez</b></p>')),
               
               fluidRow(column(width=5, offset = 1, background = 'yellow',
                               #fluidRow(box(   
-                              HTML('<h2 style="text-align:center; color:purple;"><b>Introducción</b></h2>'),
+                              HTML('<h2 style="text-align:center; color:purple;"><b>Introducci&oacuten</b></h2>'),
                               HTML('<hr style="border: 0; height: 1px; background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);"'),
                               HTML('<h4 style="text-align:justify;"></h4>'),
-                              HTML('<h4 style="text-align:justify;"> La <b>Teoría de Detección de Señales</b> (<b>SDT</b>, por sus siglas en ingl´rs), describe el problema de adaptabilidad al que se enfrentan los organismos cuando, en un entorno con incertidumbre, tienen que decidir si <i>algo</i> (la <b>señal</b>) está¡ o no presente, para poder guiar su comportamiento en función a las consecuencias asociadas a esta.</h4>'),
-                              HTML('<h4 style="text-align:justify;"> El desarrollo de la SDT gira en torno a dos grandes supuestos: <b>1) Siempre hay algún grado de incertidumbre</b> al detectar algo, por tres grandes razones: 1.- Nuestros sistemas perceptuales son imperfectos, 2.- Toda señal a detectar se presenta y percibe con cierta variabilidad y 3.- Las señales coexisten en el mundo con <b>ruido</b>: estímulos que sin ser la señal, pueden llegar a confundirse con esta; y  <b>2) Las consecuencias importan</b>, acertar en el juicio de detección paga, errar cuesta y ambas cosas ocurren en distinta magnitud.</h4>'),
-                              HTML('<h4 style="text-align:justify;"> De acuerdo con la <b>TDS</b>, los organismos ponderan la información que poseen sobre la estructura de su entorno para compensar la incertidumbre contenida en la tarea (<i>representada gráficamente como el área de sobrelape entre las distribuciones de probabilidad que describen la presentación del Ruido y la Señal</i>), fijando un <b>criterio de elección</b> (<i>la línea vertical que corta ambas distribuciones a la altura de un valor (x)</i>) que va a fungir como punto de corte para determinar a partir de cuánta evidencia se juzgará¡ la presencia de la señal.</h4>'),
-                              HTML('<h4 style="text-align: justify;">En función a la correspondencia entre la presencia o asuencia de la señal en el mundo y el juicio de elección emitido por el organismo, podemos identificar dos grandes tipos de aciertos y errores: los <b>Hits</b> ("Sí" cuando sí) y los <b>Rechazos Correctos</b> ("No" cuando no); y las <b>Falsas Alarmas</b> ("Sí" cuando no) y <b>Omisiones</b> ("No" cuando sí). Y de acuerdo con la representación gráfica de la SDT, la probabilidad de cometer cada uno de estos aciertos y errores estará¡ determinada por el área de las distribuciones de Ruido y Señal que caigan a cada lado del criterio de elección, según sea el caso.</h3>'),
+                              HTML('<h4 style="text-align:justify;"> La <b>Teor&iacutea de Detecci&oacuten de Se&ntildeales</b> (<b>TDS</b>), proporciona un modelo estadístico para entender el problema al que se enfrentan los organismos cuando tienen que decidir si <i>algo</i> (una <b>se&ntildeal</b> a detectar) est&aacute o no ocurriendo, para poder guiar su comportamiento en funci&oacuten a las consecuencias anunciadas.</h4>'),
+                              HTML('<h4 style="text-align:justify;"> La TDS gira en torno a dos grandes supuestos: <b>1) Siempre hay incertidumbre</b> porque: 1.1 Nuestros sistemas perceptuales son imperfectos, 1.2 Todo est&iacutemulo se presenta y percibe con cierta variabilidad y 1.3 Las sales coexisten en el mundo con <b>ruido</b> (est&iacutemulos que pueden llegar a confundirse con la se&ntilde); y  <b>2) Las consecuencias importan</b>, acertar en el juicio de detecci&oacuten paga, errar cuesta y ambas cosas ocurren con distinta magnitud.</h4>'),
+                              HTML('<h4 style="text-align:justify;"> De acuerdo con la <b>TDS</b>, los organismos compensan la incertidumbre con la informaci&oacuten que poseen sobre la estructura de su entorno, fijando un <b>criterio de elecci&oacuten</b> (<i>la l&iacutenea vertical que corta las distribuciones que describen la variabilidad en la presentaci&oacute en los est&iacute con ruido y se&ntilde) a la altura de un valor (x)</i>) que va a determinar a partir de cu&aacutenta evidencia se juzgar&aacute la presencia de la se&ntildeal.</h4>'),
+                              HTML('<h4 style="text-align: justify;">La localizaci&oacuten del criterio determina la probabilidad de que el organismo cometa un acierto (un <b>Hit</b> o un <b>Rechazo Correcto</b>) o un error (una <b>Falsa alarma</b> o una <b>Omisi&oacuten</b>.</h3>'),
                               tags$img(height=100, 
                                        width=100,
                                        src = "https://github.com/BouzasLab25/LabVirtual25/blob/Apps/AppsFelisa/SDT_Outcomes.jpg"),
@@ -61,40 +61,40 @@ ui <- dashboardPage(
                               HTML('<hr style="border: 0; height: 1px; background-image: -webkit-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -moz-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -ms-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0); background-image: -o-linear-gradient(left, #f0f0f0, #8c8b8b, #f0f0f0);"'),
                               HTML('<h4 style="text-align:justify;"></h4>'),
                               HTML('<h4 style="text-align:justify;">Imagina el caso de una presa que, tras escuchar un ruido tras la maleza, tiene que decidir si se trata, o no, de una amenaza.</h4>'),
-                              HTML('<h4 style="text-align:justify;">Por la experiencia que ha adquirido, esta presa sabe que 1) no siempre que un depredador se acerca, este hace el mismo ruido y 2)los depredadores no son lo ÃÂºnico que causan ruido en la maleza.</h4>'),
-                              HTML('<h4 style="text-align:justify;">Si la presa decide que el ruido podrÃÂ­a ser una amenaza y sale corriendo, pueden ocurrir dos cosas: si acierta, salva su vida y si se equivoca, habrÃÂ¡ desperdiciado energÃÂ­a que pudiese haber utilizado en buscar alimento.</h4>'),
-                              HTML('<h4 style="text-align:justify;">Por otro lado, si la presa decide que el ruido no fue una amenaza y se queda en su lugar, puede ser el caso que no gane ni pierda nada, si acierta, o en el peor de los casos, si comete una omisiÃÂ³n, muera devorado.</h4>'),
+                              HTML('<h4 style="text-align:justify;">Por la experiencia que ha adquirido, esta presa sabe que 1) no siempre que un depredador se acerca, este hace el mismo ruido y 2)los depredadores no son lo ÃÂÃÂºnico que causan ruido en la maleza.</h4>'),
+                              HTML('<h4 style="text-align:justify;">Si la presa decide que el ruido podrÃÂÃÂ­a ser una amenaza y sale corriendo, pueden ocurrir dos cosas: si acierta, salva su vida y si se equivoca, habrÃÂÃÂ¡ desperdiciado energÃÂÃÂ­a que pudiese haber utilizado en buscar alimento.</h4>'),
+                              HTML('<h4 style="text-align:justify;">Por otro lado, si la presa decide que el ruido no fue una amenaza y se queda en su lugar, puede ser el caso que no gane ni pierda nada, si acierta, o en el peor de los casos, si comete una omisiÃÂÃÂ³n, muera devorado.</h4>'),
                               plotOutput(outputId="sdt_inicial")))
                     ),
       
       # Second tab content
       tabItem(tabName = "about",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂ­a de DetecciÃÂ³n de SeÃÂ±ales</u></b></h1>'),
-                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂ¡vez</b></p>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂÃÂ­a de DetecciÃÂÃÂ³n de SeÃÂÃÂ±ales</u></b></h1>'),
+                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂÃÂ¡vez</b></p>'),
                        tags$br(),
                        tags$br()),
-              HTML('<h1 style="text-align:center;"><b>SecciÃÂ³n pendiente</b></h1>')),
+              HTML('<h1 style="text-align:center;"><b>SecciÃÂÃÂ³n pendiente</b></h1>')),
       
       tabItem(tabName = "roc",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂ­a de DetecciÃÂ³n de SeÃÂ±ales</u></b></h1>'),
-                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂ¡vez</b></p>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂÃÂ­a de DetecciÃÂÃÂ³n de SeÃÂÃÂ±ales</u></b></h1>'),
+                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂÃÂ¡vez</b></p>'),
                        tags$br(),
                        tags$br()),
-                       HTML('<h1 style="text-align:center;"><b>SecciÃÂ³n pendiente</b></h1>')),
+                       HTML('<h1 style="text-align:center;"><b>SecciÃÂÃÂ³n pendiente</b></h1>')),
       
       tabItem(tabName = "nop",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂ­a de DetecciÃÂ³n de SeÃÂ±ales</u></b></h1>'),
-                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂ¡vez</b></p>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂÃÂ­a de DetecciÃÂÃÂ³n de SeÃÂÃÂ±ales</u></b></h1>'),
+                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂÃÂ¡vez</b></p>'),
                        tags$br(),
                        tags$br()),
-              HTML('<h1 style="text-align:center;"><b>SecciÃÂ³n pendiente</b></h1>')),
+              HTML('<h1 style="text-align:center;"><b>SecciÃÂÃÂ³n pendiente</b></h1>')),
       
       tabItem(tabName = "para",
-              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂ­a de DetecciÃÂ³n de SeÃÂ±ales</u></b></h1>'),
-                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂ¡vez</b></p>'),
+              fluidRow(HTML('<h1 style="text-align:center; color:purple;"><b><u>TeorÃÂÃÂ­a de DetecciÃÂÃÂ³n de SeÃÂÃÂ±ales</u></b></h1>'),
+                       HTML('<p style="text-align:center;"><b>por Adriana F. ChÃÂÃÂ¡vez</b></p>'),
                        tags$br(),
                        tags$br()),
-              HTML('<h1 style="text-align:center;"><b>SecciÃÂ³n pendiente</b></h1>')))))
+              HTML('<h1 style="text-align:center;"><b>SecciÃÂÃÂ³n pendiente</b></h1>')))))
 
 server <- function(input, output) {
   output$sdt_inicial <- renderPlot({plot(10, 20, main="", xlab="", ylab="",type='l',
@@ -109,11 +109,11 @@ server <- function(input, output) {
     abline(v=input$crit, lwd=2)
     text(-2.9,.5,"Probabilidad de cometer un(a):",cex=1,col='black',f=2)
     text(-3.1,.46,paste("Hit= ",round(pnorm(input$crit,2,1,lower.tail=FALSE),3)), cex=1, col='forestgreen', f=2) 
-    text(-3.1,.34,paste("OmisiÃÂ³n= ",round(pnorm(input$crit,2,1,lower.tail=TRUE),3)), cex=1, col='darkorchid3', f=2) 
+    text(-3.1,.34,paste("OmisiÃÂÃÂ³n= ",round(pnorm(input$crit,2,1,lower.tail=TRUE),3)), cex=1, col='darkorchid3', f=2) 
     text(-3.1,.42,paste("Falsa Alarma= ",round(pnorm(input$crit,0,1,lower.tail=FALSE),3)), cex=1, col='firebrick3', f=2) 
     text(-3.1,.38,paste("Rechazo Correcto= ",round(pnorm(input$crit,0,1,lower.tail=TRUE),3)), cex=1, col='dodgerblue3', f=2) 
     text(0,.43,"Ruido",cex=1.5,col='black',f=2)
-    text(2,.43,"SeÃÂ±al",cex=1.5,col='black',f=2)
+    text(2,.43,"SeÃÂÃÂ±al",cex=1.5,col='black',f=2)
     mtext("Estatura de las mujeres en mi familia",1,cex=1.3, line=3, f=2)})
   
   valores <- reactive({c(input$val_1,input$val_2,input$val_3,input$val_4,input$val_5)})
